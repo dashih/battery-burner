16f6580783de6f086e2530a8e150d617b3341dfe
dss4f <danny@dannyshih.net>
2018-03-14 18:02:06 -0700

    Shows a number of progress bars depending on how many workers were activated.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
 const workers = [];
 
+function hideProgress() {
+    $('#workerProgressDiv').hide();
+    $('#stopButtonDiv').hide();
+    for (let i = 0; i < 8; i++) {
+        $('#progressBar' + i.toString()).hide();
+    }
+}
+
 function startClick() {
     $('#workersForm').hide();
     $('#workerProgressDiv').show();
+    $('#stopButtonDiv').show();
 
     let numWorkers = $('#workers').val();
     for (let i = 0; i < numWorkers; i++) {
         let worker = new Worker('js/worker.js');
         workers.push(worker);
         worker.postMessage(null);
+
+        $('#progressBar' + i.toString()).fadeIn();
     }
 }
 
 function stopClick() {
-    $('#workerProgressDiv').hide();
+    hideProgress();
     $('#workersForm').show();
     for (let i = 0; i < workers.length; i++) {
         workers[i].terminate();
@@ -23,7 +34,7 @@ function stopClick() {
 }
 
 $(document).ready(() => {
-    $('#workerProgressDiv').hide();
+    hideProgress();
     $('#startButton').click(startClick);
     $('#stopButton').click(stopClick);
 });
